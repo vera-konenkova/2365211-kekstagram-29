a70ccfc22d76b2d81ee04f66d5966f9f31783b27
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-07-05 21:46:50 +0300

    git pull academy master

---
+++ js/thumbnail.js
@@ -0,0 +1,27 @@
+const thumbnailTemplate = document
+  .querySelector('#picture')
+  .content.querySelector('.picture');
+const container = document.querySelector('.picture');
+
+const createThubmnail = ({ comments, description, likes, url }) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.querySelector('.picture__img').alt = description;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.querySelector('.picture__img').src = url;
+
+  return thumbnail;
+}
+
+const renderThumbnails = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+  const thumbnail = createThubmnail(picture);
+  console.log(thumbnail);
+  fragment.append(thumbnail);
+
+});
+
+container.append(fragment);
+};
+export {renderThumbnails};
