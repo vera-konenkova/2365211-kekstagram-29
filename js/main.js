f2ac1ad518aa3a02fa8e4fc00f01022e72c7fcfd
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-07-23 09:13:39 +0300

    Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-import { findPicture, pictures} from '/js/full-picture.js';
-
+import { findPicture, pictures } from '/js/full-picture.js';
+import {addFormChange} from '/js/form.js';
 findPicture(pictures);
+addFormChange();
