0c8154efbd3c125de7abec5893d3775b83cc75ee
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-07-23 09:21:58 +0300

    full-picture

--- js/full-picture.js
+++ js/full-picture.js
@@ -3,8 +3,8 @@ import { listOfPhotoDescriptions } from './data.js';
 
 renderThumbnails(listOfPhotoDescriptions);
 
-let COMMENT_PORTION = 5;
-let comments =[];
+const COMMENT_PORTION = 5;
+let comments = [];
 
 const bigPictureElement = document.querySelector('.big-picture');
 const commentCount = bigPictureElement.querySelector('.social__comment-count');
@@ -21,38 +21,29 @@ const createComment = ({avatar, name, message}) => {
   comment.querySelector('.social__text').textContent = message;
   return comment;
 };
-  let commentsShown = 0;
-  const onCommentsLoadClick = () => renderComments(comments);
-  const onCancelButtonClick = () => hideBigPicture();
+let commentsShown = 0;
 
-  const renderComments = (comments) => {
-    commentListElement.innerHTML = '' ;
+const renderComments = (comments) => {
+  commentListElement.innerHTML = '' ;
 
-        const commentPortion = comments.slice(commentsShown , COMMENT_PORTION + commentsShown);
-           commentsShown += COMMENT_PORTION;
+  const commentPortion = comments.slice(commentsShown , COMMENT_PORTION + commentsShown);
+  commentsShown += COMMENT_PORTION;
 
-        const fragment = document.createDocumentFragment();
-          commentPortion.forEach((item) => {
-           const comment = createComment(item);
-            fragment.append(comment);
-           });
-           commentListElement.append(fragment);
+  const fragment = document.createDocumentFragment();
+  commentPortion.forEach((item) => {
+    const comment = createComment(item);
+    fragment.append(comment);
+  });
+  commentListElement.append(fragment);
 
-    if (comments.length <= COMMENT_PORTION) {
-      commentsLoaderElement.classList.add('hidden');
-      commentCount.textContent = comments.length + ' из ' + comments.length + ' комментариев ';
-      return;
-    } else if (comments.length <= commentsShown) {
-      commentsLoaderElement.classList.add('hidden');
-      commentCount.textContent = comments.length + ' из ' + comments.length + ' комментариев ';
-      //return;
-    }
-      else {
-                commentsLoaderElement.classList.remove('hidden');
-        commentCount.textContent = commentsShown + ' из ' + comments.length + ' комментариев ';
-
-      }
-    };
+  if (comments.length <= COMMENT_PORTION || comments.length <= commentsShown) {
+    commentsLoaderElement.classList.add('hidden');
+    commentCount.textContent = `${comments.length} из ${comments.length} комментариев `;
+  } else {
+    commentsLoaderElement.classList.remove('hidden');
+    commentCount.textContent = `${commentsShown} из ${comments.length} комментариев `;
+  }
+};
 
 const hideBigPicture = () => {
   bigPictureElement.classList.add('hidden');
@@ -62,59 +53,62 @@ const hideBigPicture = () => {
   commentListElement.innerHTML = '' ;
 };
 
-function onDocumentKeydown(evt) {
- if (evt.key === 'Escape') {
-  evt.preventDefault();
-  hideBigPicture();
-  }
- }
+const onCommentsLoadClick = () => renderComments(comments);
+const onCancelButtonClick = () => hideBigPicture();
 
- const renderPictureDetails = ({ url, likes, description}) => {
+function onDocumentKeydown(evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    hideBigPicture();
+  }
+}
+
+const renderPictureDetails = ({ url, likes, description}) => {
   bigPictureElement.querySelector('.big-picture__img img').src = url;
   bigPictureElement.querySelector('.big-picture__img img').alt = description;
   bigPictureElement.querySelector('.likes-count').textContent = likes;
   bigPictureElement.querySelector('.social__caption').textContent = description;
- };
-   // const onCommentsLoadClick = () => renderComments(comments);
+};
+// const onCommentsLoadClick = () => renderComments(comments);
 
 const showBigPicture = (data) => {
   bigPictureElement.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
-      renderPictureDetails(data);
-      comments = data.comments;
-      commentListElement.innerHTML = '' ;
-      commentsShown = 0;
+  renderPictureDetails(data);
+  comments = data.comments;
+  commentListElement.innerHTML = '' ;
+  commentsShown = 0;
 
-    renderComments(comments);
+  renderComments(comments);
 
-    cancelButtonElement.removeEventListener('click', onCancelButtonClick);
-    cancelButtonElement.addEventListener('click', onCancelButtonClick);
-    commentsLoaderElement.removeEventListener('click', onCommentsLoadClick);
-    commentsLoaderElement.addEventListener('click', onCommentsLoadClick);
+  cancelButtonElement.removeEventListener('click', onCancelButtonClick);
+  cancelButtonElement.addEventListener('click', onCancelButtonClick);
+  commentsLoaderElement.removeEventListener('click', onCommentsLoadClick);
+  commentsLoaderElement.addEventListener('click', onCommentsLoadClick);
 };
 
-  // поиск картинки
- const container = document.querySelector('.pictures');
+// поиск картинки
+const container = document.querySelector('.pictures');
 
- const findPicture = (listOfPhoto) => {
-   container.addEventListener('click', (evt) => {
-     const thumbnail = evt.target.closest('[data-thumbnail-id]');
-     if (!thumbnail) {
-     return;
-     }
-     evt.preventDefault();
+const findPicture = (listOfPhoto) => {
+  container.addEventListener('click', (evt) => {
+    const thumbnail = evt.target.closest('[data-thumbnail-id]');
+    if (!thumbnail) {
+      return;
+    }
+    evt.preventDefault();
 
-     const picture = listOfPhoto.find(
-       (item) => item.id === +thumbnail.dataset.thumbnailId
-     );
-           commentListElement.innerHTML = '' ;
-      //commentsShown = 0;
+    const picture = listOfPhoto.find(
+      (item) => item.id === +thumbnail.dataset.thumbnailId
+    );
+    commentListElement.innerHTML = '' ;
+    //commentsShown = 0;
 
     showBigPicture(picture);
-   });
- };
+  });
+};
 
- const pictures = listOfPhotoDescriptions;
+const pictures = listOfPhotoDescriptions;
 
 export { pictures, findPicture};
