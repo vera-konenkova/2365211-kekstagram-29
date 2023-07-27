2820ee591c03db1d6f08f368a1b750fa269553d8
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-07-27 13:05:04 +0300

    There is galary

--- js/api.js
+++ js/api.js
@@ -1,29 +1,38 @@
-const getData = (url, onSuccess, onFail) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      onSuccess(data);
-    })
-    .catch((err) => {
-      onFail(err);
-    });
+// Получение данных
+import {showAlert} from './util.js';
+const URL = 'https://29.javascript.pages.academy/kekstagram';
+
+const Route = {
+  GET: '/data',
+  POST: '',
 };
 
-const sendData = (url, onSuccess, onFail, body) => {
-  fetch(url, {
-    method: 'POST',
-    body,
-  })
-    .then((response) => {
-      if(response.ok) {
-        onSuccess();
-      } else {
-        onFail();
-      }
-    })
-    .catch(() => {
-      onFail();
-    });
+const Method = {
+  GET: 'GET',
+  POST: 'POST'
 };
+
+const ErrorText = {
+  GET: 'Не удалось загрузить данные. Попробуйте обновить страницу',
+  POST: 'Не удалось отправить форму. Попробуйте еще раз',
+};
+
+const load = async (route, errorText, method = Method.GET, body = null) => {
+  try {
+    const response = await fetch(`${URL}${route}`, {method, body});
+    if (!response.ok) {
+      throw new Error();
+    }
+    return response.json();
+  } catch {
+    showAlert(errorText);
+  }
+};
+
+const getData = () => load(Route.GET, ErrorText.GET);
+
+const sendData = (body) => load(Route.POST, ErrorText.POST, Method.POST, body);
+
 const datas = await getData();
-export {sendData, datas};
+
+export {getData, sendData, datas};
--- js/util.js
+++ js/util.js
@@ -1,3 +1,23 @@
+const ALERT_SHOW_TIME = 5000;
+
+// Показываем ошибку
+function showAlert (message) {
+  const alert = document.createElement('div');
+  alert.style.position = 'absolute';
+  alert.style.zIndex = '100';
+  alert.style.top = '0';
+  alert.style.padding = '10px 3px';
+  alert.style.fontSize = '20px';
+  alert.style.textAlign = 'center';
+  alert.style.background = 'red';
+  alert.textContent = message;
+  document.body.append(alert);
+
+  setTimeout(() => {
+    alert.remove();
+  }, ALERT_SHOW_TIME);
+}
+
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -24,4 +44,4 @@ const getRandomArrayElement = (elements) => elements[getRandomInteger(0, element
 
 const pressEscButton = (evt) => evt.key === 'Escape';
 
-export {getRandomInteger, createRandomNumberFromRangeGenerator, getRandomArrayElement, pressEscButton};
+export {getRandomInteger, createRandomNumberFromRangeGenerator, getRandomArrayElement, pressEscButton, showAlert};
