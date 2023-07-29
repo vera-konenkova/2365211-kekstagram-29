5dad54c878bc6a188652acc5d754c8617684ca9c
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-07-29 10:12:28 +0300

    photo-filters

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 import { findPicture, pictures } from '/js/full-picture.js';
 import { addFormChange } from '/js/form.js';
 import { resetScale } from '/js/scale.js';
-
 //import './form.js';
 import './form-messages.js';
-
 findPicture(pictures);
-
-
 addFormChange();
-
 resetScale();
+import { renderSortedPosts } from './photo-filter.js';
+renderSortedPosts();
