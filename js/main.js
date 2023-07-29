921024ffb62da862b8c2f86eaecd97cb3ac80fb8
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-07-29 15:19:23 +0300

    Image is uploading

--- js/main.js
+++ js/main.js
@@ -5,11 +5,13 @@ import { findPicture, pictures } from '/js/full-picture.js';
 import { addFormChange } from '/js/form.js';
 import { resetScale } from '/js/scale.js';
 
-//import './form.js';
+import './form.js';
+
 import './form-messages.js';
+
 findPicture(pictures);
 addFormChange();
 resetScale();
-
+import './uploading-photo.js';
 // import { renderSortedPosts } from './photo-filter.js';
 // renderSortedPosts();
---
+++ js/uploading-photo.js
@@ -0,0 +1,19 @@
+// Загрузка изображения пользователя
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+
+const fileChooser = document.querySelector('.img-upload__input');
+const scaleImage = document.querySelector('.img-upload__preview');
+const preview = scaleImage.querySelector('img');
+const smallPreviews = document.querySelectorAll('.effects__preview');
+
+fileChooser.addEventListener('change', () => {
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((type) => fileName.endsWith(type));
+  if (matches) {
+    preview.src = URL.createObjectURL(file);
+    smallPreviews.forEach((smallPreview) => {
+      smallPreview.style.backgroundImage = `  url('${preview.src}')`;
+    });
+  }
+});
