036a2653a0e469716c8cb0ea135684953c268dfe
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-07-30 20:31:06 +0300

    Фото на память

--- js/util.js
+++ js/util.js
@@ -38,17 +38,23 @@ const createRandomPosts = (array) => {
 };
 
 // «устранение дребезга»
-function debounce (callback, timeoutDelay) {
-  let isCooldown = false;
-  return function() {
-    if (isCooldown) {
-      return;
-    }
-    callback.apply(this, arguments);
-    isCooldown = true;
-    setTimeout(() => {
-      isCooldown = false;
-    }, timeoutDelay);
+// Источник - https://www.freecodecamp.org/news/javascript-debounce-example
+
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
   };
 }
 
